dafc79d8599cb5f2d72c23b044c3b641daee54cf
Kyura <96605016+Kyuraxp@users.noreply.github.com>
2022-01-31 18:24:55 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -114,7 +114,7 @@ GITHUB_ACCESS_TOKEN = os.environ.get("GITHUB_ACCESS_TOKEN", None)
 UPSTREAM_REPO_URL = os.environ.get(
     "UPSTREAM_REPO_URL", "https://github.com/Kyuraxp/kyura-userbot"
 )
-UPSTREAM_REPO_BRANCH = os.environ.get("UPSTREAM_REPO_BRANCH", "Skyzuu-Userbot")
+UPSTREAM_REPO_BRANCH = os.environ.get("UPSTREAM_REPO_BRANCH", "Kyura-Userbot")
 
 # Console verbose logging
 CONSOLE_LOGGER_VERBOSE = sb(os.environ.get("CONSOLE_LOGGER_VERBOSE", "False"))
