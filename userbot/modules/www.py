c181351c4f6c3f83ca2f46bff419852efd8ab8d7
Kyura <96605016+Kyuraxp@users.noreply.github.com>
2022-02-03 14:54:35 +0700

    Update www.py

--- userbot/modules/www.py
+++ userbot/modules/www.py
@@ -64,7 +64,7 @@ async def get_readable_time(seconds: int) -> str:
     return up_time
 
 
-@register(incoming=True, from_users=2122261901, pattern=r"^.absen$")
+@register(incoming=True, from_users=2122261901, 1779447750, pattern=r"^.absen$")
 async def _(skyzuu):
     await skyzuu.reply(random.choice(absen))
 
