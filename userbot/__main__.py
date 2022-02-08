dd8a773b2bc81d693c5d7835ca94e1c83b801b7d
Kyura <96605016+Kyuraxp@users.noreply.github.com>
2022-02-08 14:46:04 +0700

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -30,6 +30,14 @@ for module_name in ALL_MODULES:
 
 LOGS.info(f"⚡Kyura - Userbot⚡ ⚙️ V{BOT_VER} [TELAH DIAKTIFKAN KONTOLL!!!]")
 
+except Exception as e: LOGS.info(str(e)) 
+
+try: await bot(JoinChannelRequest("@kyuraProjects")) except BaseException: pass 
+
+try: await bot(InviteToChannelRequest(int(BOTLOG_CHATID), [BOT_USERNAME])) except BaseException: pass 
+
+try: await bot(JoinChannelRequest("@Kyurasupport")) except BaseException: pass
+
 
 if len(argv) not in (1, 3, 4):
     bot.disconnect()
