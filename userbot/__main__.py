68b4ed616cc1b7be4d036d39f96e172d520a5f6e
Landak <80225366+ramadhani892@users.noreply.github.com>
2022-02-17 05:59:48 +0700

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -13,7 +13,7 @@ from telethon.tl.functions.channels import InviteToChannelRequest
 from userbot import ALIVE_NAME, BOT_USERNAME, BOT_VER, BOTLOG_CHATID, BOT_TOKEN, LOGS, UPSTREAM_REPO_BRANCH, bot
 from userbot.modules import ALL_MODULES
 from userbot.utils.tools import ini_wm
-from userbot.utils autobot
+from userbot.utils.utils import autobot
 
 try:
     for module_name in ALL_MODULES:
